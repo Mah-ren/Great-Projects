d98cf0a93fa7bd1a8c8a757ff8c637541a75a815
mahmoud ahmed <om20031129@gmail.com>
2024-05-05 23:17:06 +0300

    5/5/2024 Sunday added slider cyan and working on oval one

---
+++ oval_shape_slider/oval_shape_slider.js
@@ -0,0 +1,20 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    slidesPerGroup: 1,
+    loopFillGroupWithBlank: true,
+    // loop: true,
+    loop: false,
+    grabCursor: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },  
+  });
+  
+
+  
